bb4ce107ce598fbea252ca3ca264df2795424deb
Veozen <152989587+Veozen@users.noreply.github.com>
2026-06-22 19:43:00 -0400

    Create parser_nodes.py

---
+++ parser_nodes.py
@@ -0,0 +1,90 @@
+from typing import List, Optional, Any, Union
+from dataclasses import dataclass
+
+@dataclass
+class Node:
+  line: int
+  column: int
+
+
+@dataclass
+class Statement(Node): pass
+
+@dataclass
+class StmtBlock(Node):
+    statements: List[Statement]
+
+@dataclass
+class Expr(Node):
+  tokens: List[Node] # The RPN stream
+
+@dataclass
+class Identifier(Node):
+    name: Node
+
+@dataclass
+class IdentifierList(Node):
+    identifiers: List[Identifier]
+
+@dataclass
+class SetStmt(Statement):
+  identifiers: IdentifierList    # Variable names
+  expr: Expr    # The RPN logic
+
+@dataclass
+class OutputStmt(Statement):
+  expr: Expr    # The RPN logic
+
+@dataclass
+class ErrorStmt(Statement):
+  expr: Expr    # The RPN logic
+
+@dataclass
+class ReturnStmt(Statement):
+    expr: Expr
+
+@dataclass
+class ExitStmt(Statement):
+    condition: Expr
+    expr: Expr
+
+
+@dataclass
+class FuncStmt(Statement):
+    name: str
+    args: IdentifierList
+    exit_stmt : ExitStmt
+    body_stmt: StmtBlock
+    return_stmt: ReturnStmt
+
+@dataclass
+class ImportStmt(Statement):
+  filename : Identifier
+
+@dataclass
+
+class FunctionPointer:
+    def __init__(self, func_id):
+        self.value = func_id  # Under the hood, it's still just an integer!
+
+@dataclass
+class Program:
+    source_path: str
+    #type_definitions: dict  # To store those 'type' structures
+    body: StmtBlock  # The main entry point of the code
+
+
+
+#
+# @dataclass
+# class IfStmt(Statement):
+#     condition: Expr
+#     then_block: StmtBlock
+#     else_block: StmtBlock
+#
+# @dataclass
+# class WhileStmt(Statement):
+#     cond: Expr
+#     body_block: StmtBlock
+
+
